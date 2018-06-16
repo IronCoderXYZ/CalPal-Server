557001cf1e762699e64ce65da06e9966763f2afc
Mitch Hankins <mitchhankins92@gmail.com>
2018-06-16 13:02:29 -0600

    Adding mongodb

--- config/index.js
+++ config/index.js
@@ -3,10 +3,10 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 switch (NODE_ENV) {
   case 'development':
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/CalPal';
     break;
   case 'test':
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/CalPalTest';
     break;
 }
---
+++ db/mongoose.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(uri);
+
+module.exports = mongoose;
